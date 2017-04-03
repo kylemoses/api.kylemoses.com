98d091a8474ff1a448f9d217690489be6db36709
kmoses <moses.kyle@gmail.com>
2017-04-03 01:57:28 -0400

    updates to some endpoints

--- server.js
+++ server.js
@@ -13,7 +13,10 @@ var port = process.env.PORT || 8080; // set our port
 // ROUTES FOR OUR API
 // =============================================================================
 var router = express.Router(); // get an instance of the express Router
-
+var apiData = ""
+fs.readFile(__dirname + "/" + "kyle.json", 'utf8', function(err, data) {
+	apiData = JSON.parse(data);
+});
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
 	res.json({ message: 'hooray! welcome to my api!' });
@@ -21,94 +24,82 @@ router.get('/', function(req, res) {
 
 // more routes for our API will happen here
 router.get('/:name', function(req, res) {
-	fs.readFile(__dirname + "/" + "kyle.json", 'utf8', function(err, data) {
-		var data = JSON.parse(data);
-		var dataName = data.name.substr(0, data.name.indexOf(" ")).toUpperCase();
-		var reqName = req.params.name.toUpperCase();
-		if (reqName === dataName) {
-			res.json(data);
-		} else {
-			res.status(404).send('No match for: ' + reqName);
-		}
-	});
+	var dataName = apiData.name.substr(0, apiData.name.indexOf(" ")).toUpperCase();
+	var reqName = req.params.name.toUpperCase();
+	if (reqName === dataName) {
+		res.json(apiData);
+	} else {
+		res.status(404).send('No match for: ' + reqName);
+	}
 });
 router.get('/:name/info', function(req, res) {
-	fs.readFile(__dirname + "/" + "kyle.json", 'utf8', function(err, data) {
-		var data = JSON.parse(data);
-		var dataName = data.name.substr(0, data.name.indexOf(" ")).toUpperCase();
-		var reqName = req.params.name.toUpperCase();
-		if (reqName === dataName) {
-			if (Object.keys(data.info).length >= 1) {
-				res.json(data.info);
-			} else {
-				res.status(404).send("No info found for: " + name);
-			}
+	var dataName = apiData.name.substr(0, apiData.name.indexOf(" ")).toUpperCase();
+	var reqName = req.params.name.toUpperCase();
+	if (reqName === dataName) {
+		if (Object.keys(apiData.info).length >= 1) {
+			res.json({
+				"name": apiData.name,
+				"role": apiData.role,
+				"info": apiData.info,
+				"links": apiData.links,
+			});
 		} else {
-			res.status(404).send('No match for: ' + name);
+			res.status(404).send("No info found for: " + name);
 		}
-	});
+	} else {
+		res.status(404).send('No match for: ' + name);
+	}
 });
 router.get('/:name/employed', function(req, res) {
-	fs.readFile(__dirname + "/" + "kyle.json", 'utf8', function(err, data) {
-		var data = JSON.parse(data);
-		var dataName = data.name.substr(0, data.name.indexOf(" ")).toUpperCase();
-		var reqName = req.params.name.toUpperCase();
-		if (reqName === dataName) {
-			res.json(data.employed);
-		} else {
-			res.status(404).send('No match for: ' + name);
-		}
-	});
+	var dataName = apiData.name.substr(0, apiData.name.indexOf(" ")).toUpperCase();
+	var reqName = req.params.name.toUpperCase();
+	if (reqName === dataName) {
+		res.json(apiData.employed);
+	} else {
+		res.status(404).send('No match for: ' + name);
+	}
 });
-router.get('/:name/skillset', function(req, res) {
-	fs.readFile(__dirname + "/" + "kyle.json", 'utf8', function(err, data) {
-		var data = JSON.parse(data);
-		var dataName = data.name.substr(0, data.name.indexOf(" ")).toUpperCase();
-		var reqName = req.params.name.toUpperCase();
-		if (reqName === dataName) {
-			if (Object.keys(data.skillset).length >= 1) {
-				res.json(data.skillset);
+router.get('/:name/skill_set', function(req, res) {
+	var dataName = apiData.name.substr(0, apiData.name.indexOf(" ")).toUpperCase();
+	var reqName = req.params.name.toUpperCase();
+	if (reqName === dataName) {
+		console.log('apiData.skillset: ' + apiData.skill_set);
+		if (Object.keys(apiData.skill_set).length >= 1) {
+			res.json(apiData.skill_set);
+		} else {
+			res.status(404).send(name + "has no skills!");
+		}
+	} else {
+		res.status(404).send('No match for: ' + name);
+	}
+});
+router.get('/:name/skill_set/core', function(req, res) {
+	var dataName = apiData.name.substr(0, apiData.name.indexOf(" ")).toUpperCase();
+	var reqName = req.params.name.toUpperCase();
+	if (reqName === dataName) {
+		if (Object.keys(apiData.skill_set).length >= 1) {
+			if (apiData.skill_set.core.length >= 1) {
+				res.json(apiData.skill_set.core);
 			} else {
-				res.status(404).send(name + "has no skills!");
+				res.status(404).send(name + "has no \"core\" skills!");
 			}
 		} else {
-			res.status(404).send('No match for: ' + name);
+			res.status(404).send(name + "has no skills!");
 		}
-	});
-});
-router.get('/:name/skillset/core', function(req, res) {
-	fs.readFile(__dirname + "/" + "kyle.json", 'utf8', function(err, data) {
-		var data = JSON.parse(data);
-		var dataName = data.name.substr(0, data.name.indexOf(" ")).toUpperCase();
-		var reqName = req.params.name.toUpperCase();
-		if (reqName === dataName) {
-			if (Object.keys(data.skillset).length >= 1) {
-				if (data.skillset.core.length >= 1) {
-					res.json(data.skillset.core);
-				} else {
-					res.status(404).send(name + "has no \"core\" skills!");
-				}
-			} else {
-				res.status(404).send(name + "has no skills!");
-			}
-		} else {
-			res.status(404).send('No match for: ' + name);
-		}
-	});
+	} else {
+		res.status(404).send('No match for: ' + name);
+	}
 });
 router.get('/:name/elevator_speach', function(req, res) {
-	fs.readFile(__dirname + "/" + "kyle.json", 'utf8', function(err, data) {
-		var data = JSON.parse(data);
-		var dataName = data.name.substr(0, data.name.indexOf(" ")).toUpperCase();
-		var reqName = req.params.name.toUpperCase();
-		if (reqName === dataName) {
-			if (Object.keys(data.elevator_speach).length >= 1) {
-				res.json(data.elevator_speach);
-			}
-		} else {
-			res.status(404).send('No match for: ' + name);
+	var dataName = apiData.name.substr(0, apiData.name.indexOf(" ")).toUpperCase();
+	var reqName = req.params.name.toUpperCase();
+	if (reqName === dataName) {
+		if (Object.keys(data.elevator_speach).length >= 1) {
+			res.json(data.elevator_speach);
 		}
-	});
+	} else {
+		res.status(404).send('No match for: ' + name);
+	}
 });
 
 // REGISTER OUR ROUTES -------------------------------
